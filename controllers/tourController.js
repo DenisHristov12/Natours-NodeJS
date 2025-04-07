216f977540186475ff034fcbeb572a766ba25742
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-04-07 23:57:24 +0300

    implement filtering tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,8 +2,25 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const exludedFields = ['page', 'sort', 'limit', 'field'];
 
+    exludedFields.forEach((el) => delete queryObj[el]);
+
+    // 2) Advanced Filtering
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // { difficulty: 'easy', duration: { $gte: 5 } }
+    const query = await Tour.find(JSON.parse(queryStr));
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
