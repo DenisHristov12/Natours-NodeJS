81bc6f8d1c0b653932d99cd4cc676a8064076e6c
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-05-05 16:45:23 +0300

    implement authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -95,3 +95,14 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError('You do not have permission to perform this action!', 403),
+      );
+    }
+    next();
+  };
